dc85c0dc34578386a289714c1279c7ccf92877d1
Anasslahsaini <anasslahsaini2006@gmail.com>
2026-01-16 12:51:46 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {
+        plugins: ["nativewind/babel"],
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      'react-native': 'react-native-web',
+    },
+  },
   build: {
     outDir: 'dist',
-  }
-})
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
+  define: {
+    global: 'window', 
+  },
+});
